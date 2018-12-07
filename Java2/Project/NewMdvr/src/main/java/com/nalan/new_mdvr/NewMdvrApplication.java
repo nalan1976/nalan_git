f5f42ef0358ebda7088b1a5c4d59cd3184d96f29
nalan1976 <nalan1976@790329>
2018-12-07 13:30:13 -0500

    The project first commit: delete the JPA support in build.gradle, then the project is able to run.

---
+++ Java2/Project/NewMdvr/src/main/java/com/nalan/new_mdvr/NewMdvrApplication.java
@@ -0,0 +1,12 @@
+package com.nalan.new_mdvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMdvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewMdvrApplication.class, args);
+	}
+}
