22d3d76ed8324e77b7f67e3fa73af874abdb60b6
nalan1976 <nalan1976@790329>
2019-05-17 09:37:45 -0400

    finish judge chinese character and list the difference for 2 table and add function for list 2 table, the same time to order them

--- Python/nalan/excel/excel1/access_excel.py
+++ Python/nalan/excel/excel1/access_excel.py
@@ -12,6 +12,18 @@ import re
 lst = []
 # total columns number
 TATAL_COL = 18
+# constant zero
+ZERO = '0'
+# exclude charachter
+EXCLUDE = ["", 5]
+
+
+def isAllZh(s):
+    """包含汉字的返回TRUE"""
+    for c in s:
+        if '\u4e00' <= c <= '\u9fa5':
+            return True
+    return False
 
 
 def align(row):
@@ -24,7 +36,8 @@ def align(row):
     # find the last col(that is the chinese columns) index
     idx = -1
     for i in range(0, len(row)):
-        if row[len(row) - i - 1] != "":
+        # if row[len(row) - i - 1] != "":
+        if row[len(row) - i - 1] not in EXCLUDE:
             idx = len(row) - i - 1
             break
     # insert several null string into the position
@@ -37,9 +50,16 @@ def align(row):
 def is_last_number_not_0(row):
     l = list()
     for col in row:
-        if col != "":
+        if col not in EXCLUDE:
             l.append(col)
-    if len(l) > 2 and is_number(l[-2]) and str(l[-2]) != "0.0":
+    # find chinese character
+    idx_ch = -1
+    for idx in range(0, len(l)):
+        if isAllZh(str(l[idx])):
+            idx_ch = idx
+            break
+    # whether the character before the chinese one is a no zero number
+    if idx_ch - 1 > 0 and is_number(l[idx_ch - 1]) and str(l[idx_ch - 1]) != ZERO:
         return True
     else:
         return False
@@ -61,7 +81,7 @@ def read_excel(tab):
 
     # for debug only
     idx = 0
-
+    # wash data
     for row in sheet.rows:
         for cell in row:
             if cell.value is not None:
@@ -78,17 +98,29 @@ def read_excel(tab):
     return lst
 
 
-def write_excel(path, tab):
+def write_excel(path, rows):
+    """
+    将数据写入excel的tab页中
+    """
     wb = openpyxl.Workbook()
     sheet = wb.create_sheet('demo_sheet', index=0)
 
-    for row in lst:
-        sheet.append(row)
+    # print(isinstance(rows[0], tuple))
+    # print(isinstance(rows[0], list))
+    # the rows may be a list of tuple/list or a simple list, so need the different deal with it
+    if rows is not None and not isinstance(rows[0], tuple) and not isinstance(rows[0], list):
+        sheet.append(rows)
+    else:
+        for row in rows:
+            sheet.append(row)
 
     wb.save(path)
 
 
 def modify_excel(tab):
+    """
+    给非0行后面加0
+    """
     sheet = wb.get_sheet_by_name(tab)
     x, y = -1, -1
 
@@ -105,17 +137,105 @@ def modify_excel(tab):
     wb.save("苏浩然法语单词.xlsx")
 
 
+def diff_sort(tab1, tab2):
+    sheet1 = wb.get_sheet_by_name(tab1)
+    sheet2 = wb.get_sheet_by_name(tab2)
+
+    s1 = list()
+    s2 = list()
+
+    for row in sheet1.rows:
+        for cell in row:
+            if cell.value is not None and not is_number(cell.value) and not isAllZh(cell.value):
+                s1.append(cell.value)
+                break
+    for row in sheet2.rows:
+        for cell in row:
+            if cell.value is not None and not is_number(cell.value) and not isAllZh(cell.value):
+                s2.append(cell.value)
+                break
+
+    s1.sort()
+    s2.sort()
+    ret_lst = list()
+    i = 0
+    count = max(len(s1), len(s2))
+    while i < count:
+        if i < len(s1) and i < len(s2):
+            ret_lst.append([s1[i], s2[i]])
+        elif i < len(s1):
+            ret_lst.append([s1[i], ""])
+        else:
+            ret_lst.append(["", s2[i]])
+        i += 1
+
+    return ret_lst
+
+
+def diff(tab1, tab2):
+    sheet1 = wb.get_sheet_by_name(tab1)
+    sheet2 = wb.get_sheet_by_name(tab2)
+
+    s1 = set()
+    s2 = set()
+    temp_dict = dict()
+    ret_lst = list()
+
+    for row in sheet1.rows:
+        # print(type(row))
+        for cell in row:
+            # print(type(cell))
+            if cell.value is not None and not is_number(cell.value) and not isAllZh(cell.value):
+                s1.add(cell.value)
+                temp_dict[cell.value] = row
+                break
+    for row in sheet2.rows:
+        for cell in row:
+            if cell.value is not None and not is_number(cell.value) and not isAllZh(cell.value):
+                s2.add(cell.value)
+                break
+    s3 = set()
+    s3 = s1 & s2
+
+    temp_lst = list()
+    # temp_lst = openpyxl.cell.cell.Cell(sheet1)
+    for k in s3:
+        temp_lst.append(temp_dict[k])
+    # now temp_lst include all rows needed
+    # wash data
+    for row in temp_lst:
+        in_lst = list()
+        for cell in row:
+            if cell.value is not None:
+                in_lst.append(cell.value)
+            else:
+                in_lst.append("")
+        ret_lst.append(copy.deepcopy(in_lst))
+        in_lst.clear()
+    return ret_lst
+
+
 
 wb = openpyxl.load_workbook("e:\\MyCode\\Python\\nalan\\excel\\excel1\\苏浩然法语单词.xlsx")
-# read_excel('突击1')
-# read_excel('突击2')
-# read_excel('突击3')
-# read_excel('突击4')
-# read_excel('突击5')
-# read_excel('突击6')
-# read_excel('突击7')
-# read_excel('突击8')
-modify_excel("顽固词汇2")
-# read_excel('突击9')
-# read_excel('突击2')
-# write_excel("顽固词汇2.xlsx", 0)
+# 读取非0行并汇总到表格中
+read_excel('突击1')
+read_excel('突击2')
+read_excel('突击3')
+read_excel('突击4')
+read_excel('突击5')
+read_excel('突击6')
+read_excel('突击7')
+read_excel('突击8')
+read_excel('突击9')
+# read_excel('突击10')
+write_excel("顽固词汇3.xlsx", lst)
+
+# 给非零行加0
+# modify_excel("顽固词汇2")
+
+# compare different table and output result
+write_excel("相同.xlsx", diff("顽固词汇3", "顽固词汇2"))
+
+# order the compared list and show them
+# write_excel("并列排序.xlsx", diff_sort("顽固词汇3", "顽固词汇2"))
+
